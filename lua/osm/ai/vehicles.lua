c14d3873adbde2ac85772d0d80e11538b5d59df8
Oscar <46930728+osulli@users.noreply.github.com>
2021-01-01 00:33:33 +0000

    Update vehicles.lua

--- lua/osm/ai/vehicles.lua
+++
@@ -1,8 +0,0 @@
-local Vehicles = {}
-
-function Vehicles.PreventKnockdown()
-    print("Setting: Prevent Vehicle Knockdown")
-    GameOptions.SetFloat("Crowds", "MinimumSpeedForKnockdownByCar", 99.000000) -- Prevent being knocked down by NPC Vehicles
-end
-
-return Vehicles
---
+++ scripts/braindance_protocol/ai/vehicles.lua
@@ -0,0 +1,12 @@
+require("braindance_protocol.utility")
+
+local Vehicles = {}
+
+function Vehicles.PreventKnockdown()
+    local moduleName = "Prevent Vehicle Knockdown"
+    Utilities.StartProtocol(moduleName)
+    GameOptions.SetFloat("Crowds", "MinimumSpeedForKnockdownByCar", 99.000000)
+    Utilities.FinishProtocol(moduleName)
+end
+
+return Vehicles
