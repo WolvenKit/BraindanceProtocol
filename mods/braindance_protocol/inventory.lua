32901889a6a21f61825832b0292117588c30066e
Kyle873 <kyle873@gmail.com>
2021-01-01 13:43:03 -0500

    More inventory functions

--- mods/braindance_protocol/inventory.lua
+++ mods/braindance_protocol/inventory.lua
@@ -1,7 +1,23 @@
 local Inventory = {}
 
+local ItemHashes = "braindance_protocol/data/ItemHashes.txt"
+
 function Inventory.AddItem(item, quantity)
     Game.AddToInventory(string.format("Items.%s", item), quantity)
+end
+
+function Inventory.AddAllItems()
+	for line in io.lines(ItemHashes) do
+		Game.AddToInventory(line, amount or 1)
+	end
+end
+
+function Inventory.AddFilteredItems(filter, amount)
+	for line in io.lines(ItemHashes) do
+		if string.match(line, filter) then
+			Game.AddToInventory(line, amount or 1)
+		end
+	end
 end
 
 return Inventory
