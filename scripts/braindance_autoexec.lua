f227c7925d040df2e983882751fb4a350c5e2c9f
Oscar <46930728+osulli@users.noreply.github.com>
2021-01-01 00:41:54 +0000

    Create braindance_autoexec.lua

---
+++ scripts/braindance_autoexec.lua
@@ -0,0 +1,11 @@
+--[[
+
+Add the Braindance Protocols you wish to autoexec at the start of the game
+
+To add a activate the Vehicles.PreventKnockdown protocol, add the following line:
+
+require("braindance_protocol.ai.vehicles.lua")
+Vehicles.PreventKnockdown()
+
+]]
+
