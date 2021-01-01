542490bea78df3dd3bfc2c80644182c85e30da1c
Oscar <46930728+osulli@users.noreply.github.com>
2021-01-01 16:18:30 +0000

    Update init.lua

--- mods/braindance_protocol/examples/init.lua
+++ mods/braindance_protocol/examples/init.lua
@@ -3,7 +3,7 @@ local Utilities = require("braindance_protocol.utility")
 local Example = {}
 
 function Example.Initialise()
-    local moduleName = "Hello World"
+    local moduleName = "Init"
     Utilities.StartProtocol(moduleName)
     print("Initialising Braindance Protocol")
     Utilities.FinishProtocol(moduleName)
