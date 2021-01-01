ad4a2054d4d39b4f8f72dcb398ef55bba369046e
Oscar <46930728+osulli@users.noreply.github.com>
2021-01-01 16:50:55 +0000

    Create johnny.lua

---
+++ mods/braindance_protocol/cheats/johnny.lua
@@ -0,0 +1,18 @@
+local Utilities = require("braindance_protocol.utility")
+
+local Johnny = {}
+
+function Johnny.AddItems()
+    local moduleName = "Add Johnny's Items"
+    Utilities.StartProtocol(moduleName)
+
+    Utilities.AddItem(Q005_Johnny_Glasses, 1)
+    Utilities.AddItem(q204_samurai_jacket, 1)
+    Utilities.AddItem(Q005_Johnny_Shirt, 1)
+    Utilities.AddItem(Q005_Johnny_Pants, 1)
+    Utilities.AddItem(Q005_Johnny_Shoes, 1)
+
+    Utilities.FinishProtocol(moduleName)
+end
+
+return Johnny
