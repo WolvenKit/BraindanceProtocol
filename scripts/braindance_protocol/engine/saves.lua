a5c5ce7c33fabdedb1f27a04b433a5d60d914dde
Oscar <46930728+osulli@users.noreply.github.com>
2021-01-01 00:32:04 +0000

    Update saves.lua

---
+++ scripts/braindance_protocol/engine/saves.lua
@@ -0,0 +1,28 @@
+require("braindance_protocol.utility")
+
+local Saves = {}
+
+function Saves.DisableAutosave()
+    local moduleName = "Disable Autosave"
+    Utilities.StartProtocol(moduleName)
+    GameOptions.SetFloat("SaveConfig", "AutoSaveEnabled", false)
+    Utilities.FinishProtocol(moduleName)
+end
+
+function Saves.DecreaseAutosaves()
+    local moduleName = "Decrease Autosaves"
+    Utilities.StartProtocol(moduleName)
+    GameOptions.SetFloat("SaveConfig", "AutoSaveEnabled", true)
+    GameOptions.SetFloat("SaveConfig", "AutoSavePeriod", 600)
+    Utilities.FinishProtocol(moduleName)
+end
+
+function Saves.IncreaseAutosaves()
+    local moduleName = "Decrease Autosaves"
+    Utilities.StartProtocol(moduleName)
+    GameOptions.SetFloat("SaveConfig", "AutoSaveEnabled", true)
+    GameOptions.SetFloat("SaveConfig", "AutoSavePeriod", 100)
+    Utilities.FinishProtocol(moduleName)
+end
+
+return Saves
