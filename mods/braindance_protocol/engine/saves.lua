35baca916209a2032819b799cc1f46b7adb52ffb
specik <k@specik.com>
2021-01-04 15:28:28 -0500

    Fixes #16 - Incorrect Set method used for some GameOptions

--- mods/braindance_protocol/engine/saves.lua
+++ mods/braindance_protocol/engine/saves.lua
@@ -7,15 +7,15 @@ local Utilities = require(Saves.rootPath.."utility")
 function Saves.DisableAutosave()
     local moduleName = "Disable Autosave"
     Utilities.StartProtocol(moduleName)
-    GameOptions.SetFloat("SaveConfig", "AutoSaveEnabled", false)
+    GameOptions.SetBool("SaveConfig", "AutoSaveEnabled", false)
     Utilities.FinishProtocol(moduleName)
 end
 
 function Saves.DecreaseAutosaves()
     local moduleName = "Decrease Autosaves"
     Utilities.StartProtocol(moduleName)
-    GameOptions.SetFloat("SaveConfig", "AutoSaveEnabled", true)
-    GameOptions.SetFloat("SaveConfig", "AutoSavePeriod", 600)
+    GameOptions.SetBool("SaveConfig", "AutoSaveEnabled", true)
+    GameOptions.SetInt("SaveConfig", "AutoSavePeriod", 600)
     Utilities.FinishProtocol(moduleName)
 end
 
@@ -23,14 +23,14 @@ function Saves.IncreaseAutosaves()
     local moduleName = "Increase Autosaves"
     Utilities.StartProtocol(moduleName)
     GameOptions.SetFloat("SaveConfig", "AutoSaveEnabled", true)
-    GameOptions.SetFloat("SaveConfig", "AutoSavePeriod", 100)
+    GameOptions.SetInt("SaveConfig", "AutoSavePeriod", 100)
     Utilities.FinishProtocol(moduleName)
 end
 
 function Saves.IncreaseQuicksaves()
     local moduleName = "Increase Quicksaves"
     Utilities.StartProtocol(moduleName)
-    GameOptions.SetInt("SaveSlotsConfig", "NumQuickSaveSlots", 10)
+    GameOptions.SetInt("Developer/SaveSlotsConfig", "NumQuickSaveSlots", 10)
     Utilities.FinishProtocol(moduleName)
 end
 
--- mods/braindance_protocol/engine/visual.lua
+++ mods/braindance_protocol/engine/visual.lua
@@ -6,7 +6,7 @@ function Visual.Rain(state)
     local moduleName = "Toggle Rain"
     Utilities.StartProtocol(moduleName)
     state = state or true
-    GameOptions.SetFloat("FeatureToggles", "RainMap", true)
+    GameOptions.SetBool("Developer/FeatureToggles", "RainMap", state)
     Utilities.FinishProtocol(moduleName)
 end
 
