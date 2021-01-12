05c8931750d5cfe26f09b997d39d17e04ba76a73
VP-EN <67713433+VP-EN@users.noreply.github.com>
2021-01-12 22:28:22 +0100

    Update crafting.lua with all legendary recipe items
    I updated the crafting.lua file with every available legendary recipe in the game. 
    At least the ones that actually work. I named the iconic items, since that might be useful for someone?
    
    I made small categories throughout the code, should more recipes/items be added later into the game. Figured it might be easier with some categories.
    
    Note, this is only the legendary version of the recipes. There are still a lot more recipes in the game. But since the current command in the console is for legendary recipes, I figured I just wanted to add those. 
    
    I hope I didn't mess anything up. Just used the in-house Github editor.

--- mods/braindance_protocol/cheats/crafting.lua
+++ mods/braindance_protocol/cheats/crafting.lua
@@ -26,29 +26,252 @@ end
 function Crafting.AddLegendaryRecipes()
     local moduleName = "Add Legendary Crafting Recipies"
     Utilities.StartProtocol(moduleName)
-
-    Inventory.AddItem("LegendaryBootsRecipe")
-    Inventory.AddItem("LegendaryCapRecipe")
-    Inventory.AddItem("LegendaryCasualShoesRecipe")
-    Inventory.AddItem("LegendaryFormalPantsRecipe")
-    Inventory.AddItem("LegendaryFormalShoesRecipe")
-    Inventory.AddItem("LegendaryFormalSkirtRecipe")
-    Inventory.AddItem("LegendaryGlassesRecipe")
-    Inventory.AddItem("LegendaryGogglesRecipe")
-    Inventory.AddItem("LegendaryHatRecipe")
-    Inventory.AddItem("LegendaryJacketRecipe")
+    
+-- These are legendary clothing recipes
+    -- Chest Armor
     Inventory.AddItem("LegendaryJumpsuitRecipe")
-    Inventory.AddItem("LegendaryPantsRecipe")
-    Inventory.AddItem("LegendaryScarfRecipe")
     Inventory.AddItem("LegendaryShirtRecipe")
-    Inventory.AddItem("LegendaryShortsRecipe")
-    Inventory.AddItem("LegendaryTShirtRecipe")
-    Inventory.AddItem("LegendaryTechRecipe")
     Inventory.AddItem("LegendaryTightJumpsuitRecipe")
-    Inventory.AddItem("LegendaryVestRecipe")
+    Inventory.AddItem("LegendaryTShirtRecipe")
+   
+    -- Chest DLC/GOG
+    Inventory.AddItem("Recipe_GOG_DLC_TShirt_Legendary")
+    Inventory.AddItem("Recipe_GOG_Galaxy_TShirt_Legendary")
+   
+    -- Head Armor
+    Inventory.AddItem("LegendaryCapRecipe")
+    Inventory.AddItem("LegendaryHatRecipe")
+    Inventory.AddItem("LegendaryScarfRecipe")
+   
+    -- Face Armor
+    Inventory.AddItem("LegendaryGlassesRecipe")
+    Inventory.AddItem("LegendaryTechRecipe")
     Inventory.AddItem("LegendaryVisorRecipe")
+  
+    -- Feet Armor
+    Inventory.AddItem("LegendaryBootsRecipe")
     Inventory.AddItem("LegendaryCasualShoesRecipe")
+    Inventory.AddItem("LegendaryFormalShoesRecipe")
+   
+    -- Legs Armor
+    Inventory.AddItem("LegendaryFormalPantsRecipe")
+    Inventory.AddItem("LegendaryFormalSkirtRecipe")
+    Inventory.AddItem("LegendaryPantsRecipe")
+    Inventory.AddItem("LegendaryShortsRecipe")
+    
+    -- Outer Torso Armor
+    Inventory.AddItem("LegendaryJacketRecipe")
+    Inventory.AddItem("LegendaryVestRecipe")
+    Inventory.AddItem("Recipe_GOG_DLC_Jacket_Legendary")
+    Inventory.AddItem("Recipe_SQ031_Samurai_Jacket_Legendary")
 
+-- These are all the legendary guns, rifles, etc recipes
+    -- Assault Rifles
+    Inventory.AddItem("LegendaryAjaxRecipe")
+    Inventory.AddItem("LegendaryCopperheadRecipe")
+    Inventory.AddItem("LegendaryMasamuneRecipe")
+    Inventory.AddItem("LegendarySidewinderRecipe")
+    
+    -- SMGs
+    Inventory.AddItem("LegendaryDianRecipe")
+    Inventory.AddItem("LegendaryPulsarRecipe")
+    Inventory.AddItem("LegendarySaratogaRecipe")
+    Inventory.AddItem("LegendaryShingenRecipe")
+    
+    -- DB Shotguns
+    Inventory.AddItem("LegendaryIglaRecipe")
+    Inventory.AddItem("LegendaryPalicaRecipe")
+    Inventory.AddItem("LegendarySataraRecipe")
+    Inventory.AddItem("LegendaryTesteraRecipe")
+    
+    -- Shotguns
+    Inventory.AddItem("LegendaryCarnageRecipe")
+    Inventory.AddItem("LegendaryCrusherRecipe")
+    Inventory.AddItem("LegendaryTacticianRecipe")
+    Inventory.AddItem("LegendaryZhuoRecipe")
+     
+    -- Pistols
+    Inventory.AddItem("LegendaryChaoRecipe")
+    Inventory.AddItem("LegendaryKenshinRecipe")
+    Inventory.AddItem("LegendaryLexingtonRecipe")
+    Inventory.AddItem("LegendaryLibertyRecipe")
+    Inventory.AddItem("LegendaryNueRecipe")
+    Inventory.AddItem("LegendaryOmahaRecipe")
+    Inventory.AddItem("LegendaryUnityRecipe")
+    Inventory.AddItem("LegendaryYukimuraRecipe")
+    
+    -- Revolvers
+    Inventory.AddItem("LegendaryBuryaRecipe")
+    Inventory.AddItem("LegendaryNovaRecipe")
+    Inventory.AddItem("LegendaryOvertureRecipe")
+    Inventory.AddItem("LegendaryQuasarRecipe")
+   
+    -- Precision Rifles
+    Inventory.AddItem("LegendaryAchillesRecipe")
+    Inventory.AddItem("LegendarySor22Recipe")
+    
+    -- Sniper Rifles
+    Inventory.AddItem("LegendaryAshuraRecipe")
+    Inventory.AddItem("LegendaryGradRecipe")
+    Inventory.AddItem("LegendaryNekomataRecipe")
+
+-- Melee Weapons
+    -- Blade weapons
+    Inventory.AddItem("LegendaryButchersKnifeRecipe")
+    Inventory.AddItem("LegendaryChefsKnifeRecipe")
+    Inventory.AddItem("LegendaryKatanaRecipe")
+    Inventory.AddItem("LegendaryKnifeRecipe")
+    Inventory.AddItem("LegendaryKukriRecipe")
+    Inventory.AddItem("LegendaryMacheteRecipe")
+    Inventory.AddItem("LegendaryTantoRecipe")
+    Inventory.AddItem("Recipe_Preset_Katana_GoG_Legendary")
+   
+    
+    -- Blunt weapons
+    Inventory.AddItem("LegendaryBatonAlphaRecipe")
+    Inventory.AddItem("LegendaryBatRecipe")
+    Inventory.AddItem("LegendaryCrowbarRecipe")
+    Inventory.AddItem("LegendaryHammerRecipe")
+    Inventory.AddItem("LegendaryKanaboRecipe")
+    Inventory.AddItem("LegendaryPipeWrenchRecipe")
+    Inventory.AddItem("LegendaryTireIronRecipe")
+    Inventory.AddItem("LegendaryTomahawkRecipe")
+
+    
+-- Legendary Clothing Mods
+    Inventory.AddItem("Recipe_PowerfulFabricEnhancer01")
+    Inventory.AddItem("Recipe_PowerfulFabricEnhancer03")
+    Inventory.AddItem("Recipe_PowerfulFabricEnhancer07")
+    Inventory.AddItem("Recipe_PowerfulFabricEnhancer08")
+    Inventory.AddItem("Recipe_SimpleFabricEnhancer03")
+    Inventory.AddItem("Recipe_SimpleFabricEnhancer04")
+
+-- Legendary Quickhack Mods
+    Inventory.AddItem("Recipe_BlindLvl4Program")
+    Inventory.AddItem("Recipe_BrainMeltLvl4Program")
+    Inventory.AddItem("Recipe_CommsNoiseLvl4Program")
+    Inventory.AddItem("Recipe_ContagionLvl4Program")
+    Inventory.AddItem("Recipe_EMPOverloadLvl4Program")
+    Inventory.AddItem("Recipe_GrenadeExplodeLvl4Program")
+    Inventory.AddItem("Recipe_LocomotionMalfunctionLvl4Program")
+    Inventory.AddItem("Recipe_MadnessLvl4Program")
+    Inventory.AddItem("Recipe_OverheatLvl4Program")
+    Inventory.AddItem("Recipe_PingLvl4Program")
+    Inventory.AddItem("Recipe_SuicideLvl4Program")
+    Inventory.AddItem("Recipe_WeaponMalfunctionLvl4Program")
+
+-- "Iconic" (unique) weapons tagged as legendary
+    -- Gold Plated Baseball Bat
+    Inventory.AddItem("Recipe_Preset_Baseball_Bat_Denny_Legendary")
+    
+    -- Widow Maker
+    Inventory.AddItem("Recipe_Preset_Achilles_Nash_Legendary")
+    
+    -- Moron Labe
+    Inventory.AddItem("Recipe_Preset_Ajax_Moron_Legendary")
+    
+    -- Tinker Bell
+    Inventory.AddItem("Recipe_Preset_Baton_Tinker_Bell_Legendary")
+    
+    -- Comrade's Hammer
+    Inventory.AddItem("Recipe_Preset_Burya_Comrade_Legendary")
+    
+    -- Cottonmouth
+    Inventory.AddItem("Recipe_Preset_Cane_Fingers_Legendary")
+    
+    -- Mox
+    Inventory.AddItem("Recipe_Preset_Carnage_Mox_Legendary")
+    
+    -- PSALM 11:16
+    Inventory.AddItem("Recipe_Preset_Copperhead_Genesis_Legendary")
+    
+    -- Sir John Phallustiff
+    Inventory.AddItem("Recipe_Preset_Dildo_Stout_Legendary")
+    
+    -- O'Five
+    Inventory.AddItem("Recipe_Preset_Grad_Buck_Legendary")
+    
+    -- Overwatch
+    Inventory.AddItem("Recipe_Preset_Grad_Panam_Legendary")
+    
+    -- Sovereign
+    Inventory.AddItem("Recipe_Preset_Igla_Sovereign_Legendary")
+    
+    -- Cocktail Stick
+    Inventory.AddItem("Recipe_Preset_Katana_Cocktail_Legendary")
+    
+    -- Tsumetogi
+    Inventory.AddItem("Recipe_Preset_Katana_Hiromi_Legendary")
+    
+    -- Satori
+    Inventory.AddItem("Recipe_Preset_Katana_Saburo_Legendary")
+    
+    -- Scalpel
+    Inventory.AddItem("Recipe_Preset_Katana_Surgeon_Legendary")
+    
+    -- Jinchu-Maru
+    Inventory.AddItem("Recipe_Preset_Katana_Takemura_Legendary")
+    
+    -- Apparition
+    Inventory.AddItem("Recipe_Preset_Kenshin_Frank_Legendary")
+    
+    -- Chaos
+    Inventory.AddItem("Recipe_Preset_Kenshin_Royce_Legendary")
+    
+    -- Stinger
+    Inventory.AddItem("Recipe_Preset_Knife_Stinger_Legendary")
+    
+    -- Dying Night
+    Inventory.AddItem("Recipe_Preset_Lexington_Wilson_Legendary")
+    
+    -- Plan B
+    Inventory.AddItem("Recipe_Preset_Liberty_Dex_Legendary")
+    
+    -- Kongou
+    Inventory.AddItem("Recipe_Preset_Liberty_Yorinobu_Legendary")
+    
+    -- Breakthrough
+    Inventory.AddItem("Recipe_Preset_Nekomata_Breakthrough_Legendary")
+    
+    -- Doom Doom
+    Inventory.AddItem("Recipe_Preset_Nova_Doom_Doom_Legendary")
+    
+    -- La Chingona Dorada
+    Inventory.AddItem("Recipe_Preset_Nue_Jackie_Legendary")
+    
+    -- Death & Taxes
+    Inventory.AddItem("Recipe_Preset_Nue_Maiko_Legendary")
+    
+    -- Lizzie
+    Inventory.AddItem("Recipe_Preset_Omaha_Suzie_Legendary")
+    
+    -- Amnesty
+    Inventory.AddItem("Recipe_Preset_Overture_Cassidy_Legendary")
+    
+    -- Archangel
+    Inventory.AddItem("Recipe_Preset_Overture_Kerry_Legendary")
+    
+    -- Crash
+    Inventory.AddItem("Recipe_Preset_Overture_River_Legendary")
+    
+    -- Buzzsaw
+    Inventory.AddItem("Recipe_Preset_Pulsar_Buzzsaw_Legendary")
+    
+    -- Fenrir
+    Inventory.AddItem("Recipe_Preset_Saratoga_Maelstrom_Legendary")
+    
+    -- Problem Solver
+    Inventory.AddItem("Recipe_Preset_Saratoga_Raffen_Legendary")
+    
+    -- Divided We Stand
+    Inventory.AddItem("Recipe_Preset_Sidewinder_Divided_Legendary")
+    
+    -- The Headsman
+    Inventory.AddItem("Recipe_Preset_Tactician_Headsman_Legendary")
+    
+    -- Genjiroh
+    Inventory.AddItem("Recipe_Preset_Yukimura_Kiji_Legendary")    
+    
     Utilities.FinishProtocol(moduleName)
 end
 
