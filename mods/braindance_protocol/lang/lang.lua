9cdabecaa39516fb44ee9b6c0a97a8e04c70480b
Nats-ji <angel_tommy@foxmail.com>
2021-01-27 18:43:32 +0800

    Updated some translations

--- mods/braindance_protocol/lang/lang.lua
+++ mods/braindance_protocol/lang/lang.lua
@@ -2,6 +2,5 @@ return {
   { id = "en", name = "English" },
   { id = "cn", name = "中文 (Chinese)"},
   { id = "jp", name = "日本語 (Japanese)"},
-  { id = "de", name = "Deutsche (German)"},
   { id = "da", name = "Dansk (Danish)"}
 }
