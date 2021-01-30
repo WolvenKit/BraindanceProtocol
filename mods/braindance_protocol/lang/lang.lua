ad2613352fd29c61da16f1f63b87b3166bb4f17f
Dominik Gedon <dominik@gedon.org>
2021-01-30 19:58:57 +0100

    Add German language part II

--- mods/braindance_protocol/lang/lang.lua
+++ mods/braindance_protocol/lang/lang.lua
@@ -2,6 +2,6 @@ return {
   { id = "en", name = "English" },
   { id = "cn", name = "中文 (Chinese)"},
   { id = "da", name = "Dansk (Danish)"},
-  { id = "ru", name = "Русский (Russian)"}
+  { id = "ru", name = "Русский (Russian)"},
   { id = "de", name = "Deutsch (German)"}
 }
