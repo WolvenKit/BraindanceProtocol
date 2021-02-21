8377e39aa94d21776ff6a7d4b25e7b4eabad91ac
nats-ji <angel_tommy@foxmail.com>
2021-02-21 09:10:17 +0800

    Modularize ui list

--- mods/braindance_protocol/ui/searchbar.lua
+++ mods/braindance_protocol/ui/searchbar.lua
@@ -1,8 +1,11 @@
 local CPS = require("CPStyling")
 local theme = CPS.theme
-local color = CPS.color
 local search = require("ui/search")
 local i18n = require("i18n")
+local languages = require("lang/lang")
+local langUpdate = require("lang/update")
+local Options = require("options")
+
 local searchbar = {
   seleted = false,
   textbefore = "",
@@ -19,12 +22,14 @@ function searchbar:onTextChange()
 end
 
 function searchbar:Draw()
-  ImGui.PushItemWidth(200)
+  local winWidth = ImGui.GetWindowSize()
+  ImGui.Indent(3)
+  ImGui.PushItemWidth(winWidth*0.7)
   self.text, self.seleted = ImGui.InputTextWithHint("##searchbar", i18n("searchbar_hint"), self.text, 20)
   ImGui.PopItemWidth()
   if self.text ~= "" then
     ImGui.SetItemAllowOverlap()
-    ImGui.SameLine(190)
+    ImGui.SameLine(winWidth*0.7 - ImGui.CalcTextSize("X")-4)
     CPS.colorBegin("Button", theme.Hidden)
     if ImGui.Button("X") then
       self.text = ""
@@ -32,8 +37,41 @@ function searchbar:Draw()
     end
     CPS.colorEnd(1)
   end
+  ImGui.Unindent(3)
   if self:onTextChange() then
     search:query(self.text)
+  end
+end
+
+function searchbar.DrawLang()
+  local winWidth = ImGui.GetWindowSize()
+  ImGui.SameLine(winWidth - ImGui.CalcTextSize(i18n("button_language")) - 10)
+  ImGui.Text(i18n("button_language"))
+  if ImGui.IsItemHovered() then
+    ImGui.SetTooltip(i18n("button_language_tooltip"))
+  end
+  if ImGui.IsItemClicked() then
+    ImGui.OpenPopup("Language")
+  end
+  if ImGui.BeginPopup("Language") then
+    if Options.value.debug then
+      if ImGui.Button(i18n("button_update_lang")) then
+        langUpdate()
+      end
+    end
+    for l in pairs(languages) do
+      languages[l].selLang = ImGui.Selectable(languages[l].name, false)
+    end
+    ImGui.EndPopup()
+  end
+end
+
+function searchbar.UpdateLang()
+  for l in pairs(languages) do
+    if languages[l].selLang then
+      Options:setLang(languages[l].id)
+      languages[l].selLang = false
+    end
   end
 end
 
--- mods/braindance_protocol/ui/searchlist.lua
+++ mods/braindance_protocol/ui/searchlist.lua
@@ -1,35 +1,31 @@
 local i18n = require("i18n")
 local CPS = require("CPStyling")
 local search = require("ui/search")
-
+local list = require("ui/list")
 local searchlist = {}
 
-function searchlist:Draw()
+function searchlist:Update()
   for i,v in ipairs(search.result) do
-    local btnWidth = 130
-    ImGui.BeginGroup()
-    ImGui.PushID(i)
-    if v.type == "Button" then
-      v.press = CPS.CPButton(i18n(v.button_label), btnWidth, 0)
-    elseif v.type == "Input" then
-      ImGui.PushItemWidth(btnWidth*2/3-2)
-      v.value = ImGui.InputInt("##input" , v.value, 0)
-      ImGui.PopItemWidth()
-      ImGui.SameLine(btnWidth*2/3)
-      v.press = CPS.CPButton(i18n(v.button_label), btnWidth/3, 0)
-    elseif v.type == "Toggle" then
-      v.value, v.press = CPS.CPToggle( nil, i18n(v.button_label1), i18n(v.button_label2), v.value, btnWidth, 0)
-    elseif v.type == "Select" then
-      ImGui.PushItemWidth(btnWidth)
-      v.value, v.press = ImGui.Combo("##select", v.value, i18n(v.options))
-      ImGui.PopItemWidth()
+    if v.press then
+      if v.type ~= "Button" and v.value ~= nil then
+        v.cmd(v.value)
+      else
+        v.cmd()
+      end
     end
-    ImGui.SameLine()
-    ImGui.Text(i18n(v.name))
-    ImGui.PopID()
-    ImGui.EndGroup()
-    if ImGui.IsItemHovered() then
-      ImGui.SetTooltip(i18n(v.description))
+  end
+end
+
+function searchlist:Draw()
+  if #search.result == 0 then
+    ImGui.Indent(3)
+    ImGui.Text(i18n("text_searchlist_no_result"))
+    ImGui.Unindent(3)
+  else
+    for i,v in ipairs(search.result) do
+      ImGui.PushID(i)
+      list.DrawItem(v)
+      ImGui.PopID()
     end
   end
 end
